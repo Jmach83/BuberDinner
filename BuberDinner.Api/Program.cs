8c32d83324fc268afe50ef6c1cdb12b6708f9e5c
Jmach <jmach83@gmail.com>
2023-02-13 19:17:43 +0100

    Part 4 - Global Error Handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,6 @@
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -7,11 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    // builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+
+    // app.Map("/error", (HttpContext httpContext) =>
+    // {
+    //     Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+    //
+    //     return Results.Problem();
+    // });
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
