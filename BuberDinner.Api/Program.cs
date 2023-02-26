873e04bfb1f831b9ad99e9f1db94c8cd2f579dbd
Jmach <jmach83@gmail.com>
2023-02-26 17:42:09 +0100

    Part 5 - Flow control

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,8 @@
+using BuberDinner.Api.Common.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,23 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
-    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
 
-    // builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
-    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseExceptionHandler("/error");
-
-    // app.Map("/error", (HttpContext httpContext) =>
-    // {
-    //     Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-    //
-    //     return Results.Problem();
-    // });
 
     app.UseHttpsRedirection();
     app.MapControllers();
